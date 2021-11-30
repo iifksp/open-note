9ba086c2bf74821d15f21b9b54b9f50967aec242
Sword Wang <iifksp@swordair.com>
2021-12-01 00:05:26 +0800

    Create bilibili-t-comment-raffle.js

---
+++ bilibili-t-comment-raffle.js
@@ -0,0 +1,58 @@
+(async function () {
+  // config
+  const oid = "598864860801381543";
+
+  // max-page size
+  let max = 10;
+
+  let data = [];
+  let next = 0;
+  let end = false;
+  let participant = [];
+
+  // record uid
+  function record(replies) {
+    for (let i = 0; i < replies.length; i++) {
+      participant.push(replies[i].mid);
+      if (replies[i].replies) {
+        record(replies[i].replies);
+      }
+    }
+  }
+
+  // random function
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  // fetch data
+  while (!end && max > 0) {
+    console.log(`starting fetch page ${next + 1}`);
+    const page = await fetch(
+      `https://api.bilibili.com/x/v2/reply/main?next=${next}&type=17&oid=${oid}`
+    ).then((response) => response.json());
+    if (page && page.data && page.data.replies) {
+      record(page.data.replies);
+    }
+    if (page.data.cursor.is_end) {
+      end = true;
+    }
+    next++;
+    max--;
+  }
+
+  // dedup
+  const uniqueParticipant = Array.from(new Set(participant));
+
+  // output participant number
+  console.log("Participant num:", uniqueParticipant.length);
+
+  // get random index
+  const randon = getRandomInt(uniqueParticipant.length);
+
+  // output win uid
+  console.log(
+    "%cWin uid:" + uniqueParticipant[randon],
+    "color: red;font-size: 24px;font-weight: bold;"
+  );
+})();
